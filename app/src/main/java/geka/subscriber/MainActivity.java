79ace0a3bf66a3cd5254877ccbbb9d7bb8c4e3e9
Kyriakos Kyriakou <kyriakosb4a@yahoo.gr>
2018-06-21 19:22:35 +0300

    Navigation Menu fragments initialization

--- app/src/main/java/geka/subscriber/MainActivity.java
+++ app/src/main/java/geka/subscriber/MainActivity.java
@@ -1,15 +1,17 @@
 package geka.subscriber;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private TextView mTextMessage;
+    //private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_discover);
+                case R.id.navigation_discover:
+                    //mTextMessage.setText(R.string.title_discover);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_this_week);
+                case R.id.navigation_this_week:
+                    //mTextMessage.setText(R.string.title_this_week);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_search);
+                case R.id.navigation_search:
+                    //mTextMessage.setText(R.string.title_search);
                     return true;
             }
             return false;
@@ -36,9 +38,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //mTextMessage = (TextView) findViewById(R.id.message);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new DiscoverFragment());
     }
 
+    private boolean loadFragment(Fragment fragment){
+
+        if(fragment!=null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.navigation_discover:
+                fragment = new DiscoverFragment();
+                break;
+            case R.id.navigation_this_week:
+                fragment = new ThisWeekFragment();
+                break;
+            case R.id.navigation_search:
+                fragment = new SearchFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
 }
